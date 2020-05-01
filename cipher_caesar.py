60d89f41090afa310b6380f4610ad7ab3f525c8b
Wellington Menezes <ligton.ribeiro@gmail.com>
2020-05-01 01:45:12 -0300

    Atualização arquivo do arquivo JSON e SHA1

--- cipher_caesar.py
+++ cipher_caesar.py
@@ -1,17 +1,18 @@
-import json 
-import requests
+import json, requests, hashlib
 
+# Consumindo a API, salvando reposta em arquivo JSON e deserializado JSON
 url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=57b8d8c5faf6f2dd86613eedf7ae8aca34bb290f"
 r = requests.get(url)
 response_json = r.json()
 
-with open('answer.json', 'w') as json_file:
-    json.dump(response_json, json_file, indent=4)
+with open('answer.json', 'w') as arquivo_json:
+    json.dump(response_json, arquivo_json, indent=4)
  
 
 with open('answer.json', 'r') as json_deserialize:
     dados = json.load(json_deserialize)
 
+# Descriptografando o texto.
 cifrado = dados['cifrado']
 numero_casas = dados['numero_casas']
 caracteres_validos = 'abcdefghijklmnopqrstuvwxyz'
@@ -25,4 +26,19 @@ for letra in cifrado:
     else:
         decifrado = decifrado + letra
 
-print(decifrado)
+print("Texto Cifrado: "+ cifrado)
+print("Texto Decifrado: "+ decifrado)
+
+# Atualizando arquivo JSON
+dados['decifrado'] = decifrado
+
+with open('answer.json', 'w') as atualizar_cifrado_json:
+    json.dump(dados, atualizar_cifrado_json, indent=4)
+
+# SHA1
+resumo_decifrado = dados['decifrado']
+resumo_cripografico = hashlib.sha1(resumo_decifrado.encode('utf-8')).hexdigest()
+dados['resumo_cripografico'] = resumo_cripografico
+
+with open('answer.json', 'w') as atualizar_resumo_json:
+    json.dump(dados, atualizar_resumo_json, indent=4)
