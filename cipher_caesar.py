868f441834646ecb35291c5bfed72abeec8aeb26
Wellington Ribeiro <ligton.ribeiro@gmail.com>
2020-05-01 19:56:53 -0300

    Alterações

--- cipher_caesar.py
+++ cipher_caesar.py
@@ -1,14 +1,16 @@
-import json, requests, hashlib
+import hashlib
+import json
+import requests
+
 from decouple import config
 
 # Consumindo a API, salvando reposta em arquivo JSON
-url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token="+config('TOKEN')
+url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=" + config('TOKEN')
 r = requests.get(url)
 response_json = r.json()
 
 with open('answer.json', 'w') as arquivo_json:
     json.dump(response_json, arquivo_json, indent=4)
- 
 
 with open('answer.json', 'r') as json_deserialize:
     dados = json.load(json_deserialize)
@@ -27,8 +29,8 @@ for letra in cifrado:
     else:
         decifrado = decifrado + letra
 
-print("Texto Cifrado: "+ cifrado)
-print("Texto Decifrado: "+ decifrado)
+print("Texto Cifrado: " + cifrado)
+print("Texto Decifrado: " + decifrado)
 
 # Atualizando arquivo JSON
 dados['decifrado'] = decifrado
